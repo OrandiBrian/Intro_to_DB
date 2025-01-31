cb046c8074ba1a6981c96af7da3385ae11213cf6
Brian Orandi <brianorandi97@gmail.com>
2025-01-31 14:40:19 +0300

    sql insert records

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- database
+USE alx_book_store;
+
+-- inserting records
+INSERT INTO Customer (customer_id, customer_name, email, addres)
+VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave");
