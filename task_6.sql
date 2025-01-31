0a1d2613d2cb6dfec17160b9b12524019a64ce47
Brian Orandi <brianorandi97@gmail.com>
2025-01-31 14:45:02 +0300

    Sql multiple values

---
+++ task_6.sql
@@ -0,0 +1,8 @@
+-- database
+USE alx_book_store;
+
+-- inserting records
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES ((2, "Blessing Malik", "bmalik@sandtech.com", "123 Happiness Ave."),
+        (3, "Oben Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
+        (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave."));
