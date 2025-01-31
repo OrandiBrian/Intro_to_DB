8301d8307b2db93926342e9d56a6c3a94d354f06
Brian Orandi <brianorandi97@gmail.com>
2025-01-31 14:23:13 +0300

    Sql tables

--- task_2.sql
+++ task_2.sql
@@ -2,23 +2,23 @@
 USE alx_book_store;
 
 -- creating books table
-CREATE TABLE books (
+CREATE TABLE Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
     price DOUBLE,
     publication_date DATE,
-    CONSTRAINT fk_books_authors FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    CONSTRAINT fk_books_authors FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 -- creating authors table
-CREATE TABLE authors (
+CREATE TABLE Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
 -- creating customers table
-CREATE TABLE customers (
+CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
@@ -26,19 +26,19 @@ CREATE TABLE customers (
 );
 
 -- creating orders table
-CREATE TABLE orders (
+CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    CONSTRAINT fk_orders_customers FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    CONSTRAINT fk_orders_customers FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- creating order_details table
-CREATE TABLE order_details (
+CREATE TABLE Order_details (
     order_detailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    CONSTRAINT fk_orderdetails_orders FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    CONSTRAINT fk_orderdetails_books FOREIGN KEY (book_id) REFERENCES books(book_id)
+    CONSTRAINT fk_orderdetails_orders FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    CONSTRAINT fk_orderdetails_books FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
